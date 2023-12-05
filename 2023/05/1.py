064d37c26d04fce12ecfe9b822b09da6a1571684
joshua_2 <s6042739@live.tees.ac.uk>
2023-12-05 23:24:49 +0000

    Clean 2023/05 part 1

--- 2023/05/1.py
+++ 2023/05/1.py
@@ -14,15 +14,14 @@ for x in data[1::1]:
 
 maps_v2 = []
 for map in maps:
-    map_v2 = []
-    
+    maps_v2.append([])
+    map_v2_index = len(maps_v2)-1
     for set in map:
-        map_v2.append([set[1], set[1] + set[2] - 1, set[0], set[0] + set[2] - 1, set[2], set[0] - set[1]])    #sourceMin, sourceMax, destMin, destMax, range, diff
-    
-    maps_v2.append(map_v2)
+        maps_v2[map_v2_index].append([set[1], set[1] + set[2] - 1, set[0] - set[1]])    #sourceMin, sourceMax, diff
 
 tracked_mappings = [seeds]
-while len(maps_v2) > 0:
+mapIndex = 0
+while mapIndex < len(maps_v2):
     tracked_mappings_length = len(tracked_mappings)
     last_mappings = tracked_mappings[tracked_mappings_length - 1]
     tracked_mappings.append([])
@@ -30,16 +29,16 @@ while len(maps_v2) > 0:
     for mapping in last_mappings:
         detected_match = False
         
-        for set in maps_v2[0]:
+        for set in maps_v2[mapIndex]:
             if mapping >= set[0] and mapping <= set[1]:
                 detected_match = True
-                tracked_mappings[tracked_mappings_length].append(mapping + set[5])
+                tracked_mappings[tracked_mappings_length].append(mapping + set[2])
                 break
             
         if detected_match == False:
             tracked_mappings[tracked_mappings_length].append(mapping)
     
-    maps_v2 = maps_v2[1::1]
+    mapIndex += 1
     
 locations = tracked_mappings[len(tracked_mappings) - 1]
 locations.sort()
