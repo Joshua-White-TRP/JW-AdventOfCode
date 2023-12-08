82df94a46c350b1a6011dee4f372264881f45d6d
joshua_2 <s6042739@live.tees.ac.uk>
2023-12-08 02:22:33 +0000

    Add 2023/07 solutions

---
+++ 2023/07/1.py
@@ -0,0 +1,34 @@
+from collections import Counter
+
+data = open('Input.txt', 'r').read().split('\n')
+pairs = [x.split(' ') for x in data]
+card_priorities = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2']
+
+type_arrays = [[],[],[],[],[]] #worst to best hand 1-5 of a kind
+full_houses = []
+two_pairs = []
+
+for pair in pairs:
+    card_dict = Counter(pair[0])
+    card_values = list(card_dict.values())
+    card_values.sort(reverse=True)
+    
+    if card_values[0] == 3 and card_values[1] == 2:
+        full_houses.append(pair)
+    elif card_values[0] == 2 and card_values[1] == 2:
+        two_pairs.append(pair)
+    else:
+        type_arrays[card_values[0] - 1].append(pair)
+    
+type_arrays.insert(2, two_pairs)
+type_arrays.insert(4, full_houses)
+    
+sorted_pairs = []
+for type_array in type_arrays:
+    sorted_pairs += sorted(type_array, key=lambda x: [card_priorities.index(y) for y in x[0]], reverse=True)
+
+answer = 0
+for idx, pair in enumerate(sorted_pairs):
+    answer += (idx + 1) * int(pair[1])
+
+print(answer)
