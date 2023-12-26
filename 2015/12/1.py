1b4741430fb7aebdb6f59039e13c99ea297ff2f5
joshua_2 <s6042739@live.tees.ac.uk>
2023-12-26 14:17:46 +0000

    Add 2015/12 solutions

---
+++ 2015/12/1.py
@@ -0,0 +1,15 @@
+from json import loads
+
+def evaluate_type_switch(n):
+    t = type(n)
+    if t is str and n.isnumeric():
+        return int(n)
+    elif t is int:
+        return n
+    elif t is list:
+        return sum([evaluate_type_switch(i) for i in n])
+    elif t is dict:
+        return sum([evaluate_type_switch(n[k]) for k in n])
+    return 0
+
+print(evaluate_type_switch(loads(open('Input.txt', 'r').read())))
